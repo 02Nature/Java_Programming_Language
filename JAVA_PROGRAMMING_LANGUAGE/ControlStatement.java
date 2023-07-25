27b43a632d11f997b4ae69bfbc763a1132b71937
02Nature <rubeajuma8@gmail.com>
2023-07-25 17:38:30 +0300

    Control Structure

--- JAVA_PROGRAMMING_LANGUAGE/ControlStatement.java
+++ JAVA_PROGRAMMING_LANGUAGE/ControlStatement.java
@@ -4,7 +4,7 @@ class ControlStatement
 {
     public static void main(String[] args) 
     {
-        int x = 10;
+        int x = 6;
         //if statement
         /**********************************************
         if(x>10)
@@ -22,6 +22,44 @@ class ControlStatement
         System.out.println("End of the program");
         ************************************************/
 
+        //if-else ladder >>equivalent to switch
+        /***********************************************
+        if(x==1)
+            System.out.println("Tuma Pesa");
+        else if(x==2)
+            System.out.println("Vocha na vifurushi");
+        else if(x==3)
+            System.out.println("Toa pesa");
+        else if(x==4)
+            System.out.println("Huduma za Kibenki");
+        else if(x==5)
+            System.out.println("Malipo na bili");
+        else if(x==6)
+            System.out.println("Account yangu");
+        else
+            System.out.println("Umeingiza chaguo sio sahihi");
+        ******************************************************/
+        
+        //nested if statement
+        /****************************************************
+        int n1=2, n2=5, n3=4, greatest;
+        if(n1>n2)
+        {
+            if(n1>n3)
+                greatest = n1;
+            else
+                greatest = n3;
+        }
+        else
+        {
+            if(n2>n3)
+                greatest = n2;
+            else
+                greatest = n3;
+        }
+        System.out.println(greatest);
+        *************************************************/
+
         //while statement {>> int or string}
         /***********************************************
         while (x<30)
@@ -32,21 +70,46 @@ class ControlStatement
         *************************************************/
         //Do-while loop
         /*************************************************
+        int y=1, sum=0;
         do 
         {
-            System.out.println(x);
-            x+=2;
+           sum += y;
+           y++;
         }
-        while(x<30);
+        while(y<30);
+        System.out.println(sum);
         *************************************************/
         //for loop
         /************************************************
-        for(x=10; x<30; x+=2)
+        String[] names = {"Baba", "Mama", "Kaka","Dada"};
+        int i;
+        for(i=0; i< names.length; i++)
+            System.out.println(names[i]);
+        
+        //for-each
+         int[] arr = {1,2,3,4,5,6,7,8};
+        for(int i : arr)
         {
-            System.out.println(x);
+            if(i==5)
+            continue;
+            System.out.println(i);
+            if(i==8)
+            break;
         }
         *************************************************/
+        /**************************************************
+         * //for loop printing the matrix
+        int[][] matrix = {{1,2},{3,4}};
+        for(int i=0; i<matrix.length; i++)
+        {
+            for(int j=0; j<matrix.length; j++)
+            System.out.print("\t"+matrix[i][j]+"");
+            System.out.print("\n");
+        }
 
+        /**************************************************
+        
+        //case statement
         int y=4;
         String huduma_kwa_wateja ;
         switch(y)
@@ -60,6 +123,9 @@ class ControlStatement
             default: huduma_kwa_wateja="umeingiza chaguo sio sahihi"; break;
             
         }System.out.println(huduma_kwa_wateja);
+        *********************************************************/
+        
+        
         
 
         
